41f4fabbd275d6dea293a9bef722c7678ded0555
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2026-09-08 20:26:07 +0800

    fix: node_env problems

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,102 +1,96 @@
 const path = require("path");
 const autoprefixer = require("autoprefixer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackRootPlugin = require("html-webpack-root-plugin");
-const { type } = require("os");
+const webpack = require("webpack");
 
-module.exports = {
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  entry: { main: "./src/index.jsx", mate: "./src/mate.jsx" },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    library: "lib",
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource", // 將圖片輸出為獨立檔案
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-          },
+module.exports = (env, argv) => {
+  const mode = argv.mode || process.env.NODE_ENV || "production";
+  const isDev = mode === "development";
+
+  process.env.NODE_ENV = mode;
+
+  return {
+    target: "electron-renderer",
+    mode,
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    entry: {
+      main: "./src/index.jsx",
+      mate: "./src/mate.jsx",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].bundle.js",
+      library: "lib",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif|woff2?)$/i,
+          type: "asset/resource",
         },
-      },
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: "style-loader",
-            options: { injectType: "singletonStyleTag" },
-          },
-          "css-loader",
-        ],
-      },
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: "style-loader",
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: "css-loader",
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: "postcss-loader",
+        {
+          // 將 .js 與 .jsx 合併為一條規則即可
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
             options: {
-              postcssOptions: {
-                plugins: [autoprefixer],
-              },
+              presets: [
+                [
+                  "@babel/preset-react",
+                  {
+                    runtime: "automatic",
+                    development: isDev, // 明確同步，絕不會再有 jsxDEV 誤判
+                  },
+                ],
+                "@babel/preset-env",
+              ],
             },
           },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: "sass-loader",
-          },
-        ],
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-inline-loader",
-      },
-      {
-        test: /\.woff2?$/,
-        type: "asset/resource",
-      },
+        },
+        {
+          test: /\.s?css$/i,
+          use: [
+            {
+              loader: "style-loader",
+              options: { injectType: "singletonStyleTag" },
+            },
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [autoprefixer],
+                },
+              },
+            },
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.svg$/,
+          loader: "svg-inline-loader",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "My Electronic Mate",
+        filename: "index.html",
+        chunks: ["main"],
+      }),
+      new HtmlWebpackPlugin({
+        title: "desktop-mate",
+        filename: "mate.html",
+        chunks: ["mate"],
+      }),
+      // 明確將模式注入前端 client-side
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(mode),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "My Electronic Mate",
-      filename: "index.html",
-      chunks: ["main"],
-    }),
-    new HtmlWebpackPlugin({
-      title: "desktop-mate",
-      filename: "mate.html",
-      chunks: ["mate"],
-    }),
-  ],
+  };
 };
